5a8d8e994354097bbae28638b20b2e13f3c57734
Carina <carinalrobinson@gmail.com>
2021-03-23 12:15:20 +0100

    updated email success and fail cases

--- client/src/components/contact/contact.jsx
+++ client/src/components/contact/contact.jsx
@@ -31,6 +31,27 @@ const Contact = () =>{
             })
     }
 
+    if(result === "success"){
+        return(<div id="Contact">
+            <h1>Get in touch!</h1>
+            <p>Message successfully sent! We will be in touch shortly.</p>
+        </div>)
+    }
+
+    if(result ==="fail"){
+        return(<div id="Contact">
+            <h1>Get in touch!</h1>
+            <p>Error with message. Please check your credentials and try again.</p>
+            <p>You can either send me an email to: <a href="mailto:carinalrobinson@gmail.com">carinalrobinson@gmail.com</a> or write your query below:</p>
+        <form onSubmit={handleSubmit}>
+            <input placeholder="name" type="text" name="name" value={name} onChange={(e) =>setName(e.target.value)}></input>
+            <input placeholder="email" name="email" value={email} onChange={(e) =>setEmail(e.target.value)}></input>
+            <textarea placeholder="message" name="query" value={query} onChange={(e) =>setQuery(e.target.value)}></textarea>
+            <button type="submit">Send</button>
+        </form>
+        </div>)
+    }
+
     return(<div id="Contact">
         <h1>Get in touch!</h1>
         <p>You can either send me an email to: <a href="mailto:carinalrobinson@gmail.com">carinalrobinson@gmail.com</a> or write your query below:</p>
